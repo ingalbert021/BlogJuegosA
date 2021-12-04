2c4c67a596c2292545028cfd6e1eb40095b2309f
ingalbert021 <albertocj54@gmail.com>
2021-12-04 15:36:24 -0400

    hostin

--- src/assets/js/javaScript.js
+++ src/assets/js/javaScript.js
@@ -1,3 +1,44 @@
+/*Envio de mensajes*/
+window.addEventListener("load", function (){
+
+
+
+  emailjs.init('user_JCsxjlOoRVBBA3oCFLrby');
+
+  const btn = document.getElementById('button');
+
+  document.getElementById('form')
+   .addEventListener('submit', function(event) {
+     event.preventDefault();
+
+     btn.value = 'Sending...';
+
+     const serviceID = 'default_service';
+     const templateID = 'template_55v008d';
+
+     emailjs.sendForm(serviceID, templateID, this)
+      .then(() => {
+       const name =  document.getElementById("from_name").value = "";
+         const email = document.getElementById("to_email").value = "";
+         const message = document.getElementById("message").value = "";
+        btn.value = 'Enviado';
+        Swal.fire(
+    'Enviado correctamente!',
+    'Presiona!',
+    'success'
+  )
+      }, (err) => {
+        btn.value = 'Send Email';
+        alert(JSON.stringify(err));
+      });
+  });
+
+
+  });
+
+
+
+
 
 
 
@@ -16,44 +57,6 @@ if(window.location.href.indexOf('reloj') > -1){
 
 
 
-if(window.location.href.indexOf('inicio') > -1){
-  window.addEventListener("load", function (){
-
-    emailjs.init('user_JCsxjlOoRVBBA3oCFLrby');
-
-    const btn = document.getElementById('button');
-
-    document.getElementById('form')
-     .addEventListener('submit', function(event) {
-       event.preventDefault();
-
-       btn.value = 'Sending...';
-
-       const serviceID = 'default_service';
-       const templateID = 'template_55v008d';
-
-       emailjs.sendForm(serviceID, templateID, this)
-        .then(() => {
-         const name =  document.getElementById("from_name").value = "";
-           const email = document.getElementById("to_email").value = "";
-           const message = document.getElementById("message").value = "";
-          btn.value = 'Enviado';
-          Swal.fire(
-      'Enviado correctamente!',
-      'Presiona!',
-      'success'
-    )
-        }, (err) => {
-          btn.value = 'Send Email';
-          alert(JSON.stringify(err));
-        });
-    });
-
-
-    });
-
-
-  }
 
 
 
